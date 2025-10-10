0e74341fe81d48b73c34e096d3184fde2b10a870
Julianna Dietrich <JDietrich@scu.edu>
2025-10-10 07:38:22 -0700

    First commit

---
+++ build/CMakeFiles/RayTraceWeekend.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTraceWeekend.dir/main.cc.o"
+  "CMakeFiles/RayTraceWeekend.dir/main.cc.o.d"
+  "RayTraceWeekend"
+  "RayTraceWeekend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTraceWeekend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // image
+    int imageWidth = 256;
+    int imageHeight = 256;
+
+    // render
+
+    std::cout << "P3\n"
+              << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for (int j = 0; j < imageHeight; j++)
+    {
+        for (int i = 0; i < imageWidth; i++)
+        {
+
+            auto r = double(i) / (imageWidth - 1);
+            auto g = double(j) / (imageHeight - 1);
+            auto b = 0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
